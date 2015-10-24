888ed6689ed83c3899ef4e6b9ad45488ae453a3a
enchained <enchained@users.noreply.github.com>
2015-10-24 16:37:38 +0300

    Initial commit

---
+++ inf_sequence.py
@@ -0,0 +1,30 @@
+import sys
+
+
+def get_pos(a):
+    """
+    a: user input string to look for, numbers only.
+    ---
+    This function generates a sequence of positive integers as a string.
+    ---
+    returns: first ocurrence of 'a' in the sequence (int pos, starting from 1)
+    """
+    pass
+
+
+def main(a):
+    if a.isdigit():
+        # search sequence A position in the infinite sequence S
+        pos = get_pos(a)
+        print "Input " + a + " positioned at " + str(pos)
+    else:
+        print "Invalid input. Please enter a numeric sequence A as an argument"
+
+
+if __name__ == '__main__':
+    # read sequence A
+    if len(sys.argv) > 1:
+        main(str(sys.argv[1]))
+    else:
+        print "Error: no sequence entered."
+        print "Please enter a numeric sequence A as an argument"
